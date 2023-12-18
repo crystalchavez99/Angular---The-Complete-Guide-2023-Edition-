0bb13eff4f9a728b72c550097cfee22068897d52
Crystal <crystal120599@gmail.com>
2023-12-18 14:14:43 -0800

    completed practice!

---
+++ Basics/basics-assignment-1-start/basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "app-success-alert",
+    templateUrl: "./success-alert.component.html",
+    styles: `p{
+        color: green;
+        font-weight: bold;
+        background-color: lightgreen;
+        border: 1px solid green;
+        }`
+})
+
+export class SuccessAlertComponent {
+
+}
