e9bdec064893979ba20330cce56aef169f59726f
Crystal <crystal120599@gmail.com>
2023-12-20 13:16:28 -0800

    custom propert binding

--- Components and Databinding Deep Dive/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ Components and Databinding Deep Dive/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
   styleUrl: './game-control.component.css'
 })
-export class GameControlComponent {
+export class GameControlComponent implements OnInit{
+  @Input() number;
+  constructor() {
 
+  }
+  ngOnInit(){
+
+  }
 }
+
